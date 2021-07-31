6d02d4d9d228058591bfd5a4683bff647a23de88
tvminh19 <tvminh.it@gmail.com>
2021-07-31 16:20:56 +0700

    ud

---
+++ lib/serviceInfomation/service_information.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class MyService extends StatefulWidget {
+  const MyService({Key? key}) : super(key: key);
+
+  @override
+  _MyServiceState createState() => _MyServiceState();
+}
+
+class _MyServiceState extends State<MyService> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('service'),
+    );
+  }
+}
